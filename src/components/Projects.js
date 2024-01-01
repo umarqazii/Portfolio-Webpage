7d9d172db8d42826b0a95422aabec2eb0ff48791
Umar Qazi <i200968@nu.edu.pk>
2024-01-01 19:40:41 +0500

    changed cards background color and their button position

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,54 +12,64 @@ function Projects() {
     return (
         <div className="App" >
             <Navbar />
-            <h1 style={{fontFamily: 'Audiowide, sans-serif', color: 'white', marginTop: '50px'}}>Projects</h1>
-            <div className="CardsContainer"> {/* Custom margin-top */}
-                <Card className="Card" style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src={cardimg} />
-                    <Card.Body>
+            <h1 style={{ fontFamily: 'Audiowide, sans-serif', color: 'white', marginTop: '50px' }}>Projects</h1>
+
+            <div className="CardsContainer">
+
+                <Card className="Card" style={{ width: '18rem', background: 'grey', padding: '8px' }}>
+                    <Card.Img variant="top" src={cardimg} style={{ borderRadius: '5px' }} />
+                    <Card.Body style={{ display: 'flex', flexDirection: 'column' }}>
                         <Card.Title>Transport Booking App</Card.Title>
-                        <Card.Text style={{ fontSize: '15px' }}>
+                        <Card.Text style={{ fontSize: '13px', flex: '1 0 auto' }}>
                             A dynamic MERN (MongoDB, Express.js, React, Node.js) application, streamlining the
                             online booking process for customers.
                         </Card.Text>
-                        <Button variant="primary" href="https://github.com/umarqazii/Transport-Booking-App">Check it Out</Button>
+                        <div style={{ marginTop: 'auto', marginBottom: '5px' }}>
+                            <Button variant="primary" href="https://github.com/umarqazii/Transport-Booking-App">Check it Out</Button>
+                        </div>
                     </Card.Body>
                 </Card>
 
-                <Card className="Card" style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src={lincenseimg} />
-                    <Card.Body>
+                <Card className="Card" style={{ width: '18rem', background: 'grey', padding: '8px' }}>
+                    <Card.Img variant="top" src={lincenseimg} style={{ borderRadius: '5px' }} />
+                    <Card.Body style={{ display: 'flex', flexDirection: 'column' }}>
                         <Card.Title>License Registration App</Card.Title>
-                        <Card.Text style={{ fontSize: '15px' }}>
+                        <Card.Text style={{ fontSize: '13px', flex: '1 0 auto' }}>
                             A comprehensive MERN (MongoDB, Express.js, React, Node.js) application featuring. distinct dashboards for regular users and an admin,
                             who can manage product registrations, generate license keys, and oversee all user requests and
                             registered products.
                         </Card.Text>
+                        <div style={{ marginTop: 'auto', marginBottom: '5px' }}>
                         <Button variant="primary" href="https://github.com/umarqazii/MERN-Product-Registration-App">Check it Out</Button>
+                        </div>
                     </Card.Body>
                 </Card>
 
-                <Card className="Card" style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src={portfolioimg} />
-                    <Card.Body>
+                <Card className="Card" style={{ width: '18rem', background: 'grey', padding: '8px' }}>
+                    <Card.Img variant="top" src={portfolioimg} style={{ borderRadius: '5px' }} />
+                    <Card.Body style={{ display: 'flex', flexDirection: 'column' }}>
                         <Card.Title>Portfolio Website</Card.Title>
-                        <Card.Text style={{ fontSize: '15px' }}>
-                            A portfolio website using simple HTML, CSS, and JavaScript. Contains a single page with navigation through 
+                        <Card.Text style={{ fontSize: '13px', flex: '1 0 auto'  }}>
+                            A portfolio website using simple HTML, CSS, and JavaScript. Contains a single page with navigation through
                             scrolling.
                         </Card.Text>
+                        <div style={{ marginTop: 'auto', marginBottom: '5px' }}>
                         <Button variant="primary" href="https://www.umarqazi.me">Check it Out</Button>
+                        </div>
                     </Card.Body>
                 </Card>
 
-                <Card className="Card" style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src={cryptoimg} />
-                    <Card.Body>
-                        <Card.Title>License Registration App</Card.Title>
-                        <Card.Text style={{ fontSize: '15px' }}>
-                        App designed made using Java/JavaFX to learn about cryptocurrencies and providing the necessary tools 
-                        to help you in your trading journey
+                <Card className="Card" style={{ width: '18rem', background: 'grey', padding: '8px' }}>
+                    <Card.Img variant="top" src={cryptoimg} style={{ borderRadius: '5px' }} />
+                    <Card.Body style={{ display: 'flex', flexDirection: 'column' }}>
+                        <Card.Title>Crypto Portfolio Tracking App</Card.Title>
+                        <Card.Text style={{ fontSize: '13px', flex: '1 0 auto'  }}>
+                            App designed made using Java/JavaFX to learn about cryptocurrencies and providing the necessary tools
+                            to help you in your trading journey
                         </Card.Text>
+                        <div style={{ marginTop: 'auto', marginBottom: '5px' }}>
                         <Button variant="primary" href="https://github.com/umarqazii/CryptoApp">Check it Out</Button>
+                        </div>
                     </Card.Body>
                 </Card>
 
