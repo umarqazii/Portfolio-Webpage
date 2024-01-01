7827d94d756da82dafc8acdbf926e4ac69ac31cb
Umar Qazi <i200968@nu.edu.pk>
2024-01-01 20:49:59 +0500

    Added Skills page

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,6 +5,8 @@ import Button from 'react-bootstrap/Button';
 
 function Skills() {
 
+    var mySkills = ['C/C++', 'Java', 'Python', 'HTML', 'CSS', 'JavaScript', 'SQL', 'MongoDB', 'Linux (Ubuntu)',
+    'React.js', 'Vue.js', 'Web Development'];
 
     return(
         <div className="App" >
@@ -12,6 +14,7 @@ function Skills() {
             <h1 style={{ fontFamily: 'Audiowide, sans-serif', color: 'white', marginTop: '50px' }}>Skills</h1>
 
             <div className="SkillsContainer">
+                
                 
             </div>
         </div>
